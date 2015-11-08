bd0f5b3b8a96d9a43f612ed3442e420aa47c58dd
davallclarke <davallclarke@gmail.com>
2015-11-08 15:44:52 -0500

    W7 all Html files add

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,16 +1,5 @@
-from django.http import HttpResponse
-def main_page(request):
-	output = '''
-	    <html>
-	        <head><title>%s</title></head>
-		<body>
-			<h1>%s</h1><p>%s</p>
-		</body>
-	    </html>
-	''' % (
-	  'Test Page',
-	  'Created By Davall Clarke',
-	  'This is a simple Hello world http test!'
+from django.http import render_to_response
+def main_page(request): return render_to_response(
+	'./index.html',{'user':'DOC'}
 	)
-	return HttpResponse(output)
 
