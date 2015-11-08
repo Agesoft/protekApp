a6f31554d15db5e99e233d82e154b592924c1010
davallclarke <davallclarke@gmail.com>
2015-11-08 16:45:01 -0500

    W7 Debugging

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,19 +1,3 @@
-	
-	'''from django.http import render_to_response
-def main_page(request): return render_to_response(
-	'index.html',{'user':'DOC'}
-	)
-	
-	
-from django.shortcuts import render_to_response
-def main_page(request):   return render_to_response(
-    '../protek/templates/index.html', 
-    { 'user': 'DOC' } 
-  )
-
-
-'''
-
 from django.http import HttpResponse
 def main_page(request):
 	output = '''
