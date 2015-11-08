bcf53e59a8293a7071adea30cbb38cf44001c7be
davallclarke <davallclarke@gmail.com>
2015-11-08 18:58:02 -0500

    W7 Debugging

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,7 +1,11 @@
-from django.http import HttpResponse
-from django.template.loader import get_template
+from django.http import HttpResponse 
+from django.template import Context 
+from django.template.loader import get_template 
 
-def main_page(request):
-    template = get_template('index.html')
-	html = template.render({'user': 'DOC'})
-    return HttpResponse(html)
+def main_page(request):   
+template = get_template('index.html')   
+variables = Context({
+    'user': 'DOC'
+  })
+output = template.render(variables)   
+return HttpResponse(output)
