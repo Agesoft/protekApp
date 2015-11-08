ccd1d0b65121e1bdeb2298412d187d735204e0ae
davallclarke <davallclarke@gmail.com>
2015-11-08 16:33:45 -0500

    W7 Debugging

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,13 +1,30 @@
-'''from django.http import render_to_response
+from django.http import HttpResponse
+def main_page(request):
+	output = '''
+	    <html>
+	        <head><title>%s</title></head>
+		<body>
+			<h1>%s</h1><p>%s</p>
+		</body>
+	    </html>
+	''' % (
+	  'Test Page',
+	  'Created By Davall Clarke',
+	  'This is a simple Hello world http test!'
+	)
+	return HttpResponse(output)
+	
+	'''from django.http import render_to_response
 def main_page(request): return render_to_response(
 	'index.html',{'user':'DOC'}
 	)
-	'''
+	
 	
 from django.shortcuts import render_to_response
 def main_page(request):   return render_to_response(
     '../protek/templates/index.html', 
-    { 'user': request.user } 
+    { 'user': 'DOC' } 
   )
 
 
+'''
