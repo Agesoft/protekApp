d44b12a9a4c841b87d5b3cce21b5a7459faffcfb
davallclarke <davallclarke@gmail.com>
2015-11-08 17:00:03 -0500

    W7 Debugging

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
+pth = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+
 from django.http import render_to_response
 def main_page(request): return render_to_response(
-	'templates/index.html',{'user':'DOC'}
+	pth,{'user':'DOC'}
 	)
