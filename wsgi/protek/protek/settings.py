b0e4719c31099e397fdffee556c33ab64971fbe3
davallclarke <davallclarke@gmail.com>
2015-11-08 17:51:43 -0500

    W7 Debugging

--- wsgi/protek/protek/settings.py
+++ wsgi/protek/protek/settings.py
@@ -75,7 +75,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
             ],
+			'loaders': [
+				'django.template.loaders.filesystem.Loader',
+				'django.template.loaders.app_directories.Loader',
+			]
         },
     },
 ]
