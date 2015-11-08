023b04b39fe42139c2f2c554cb88842f633f97e7
davallclarke <davallclarke@gmail.com>
2015-11-08 17:43:41 -0500

    W7 Debugging

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,21 +1,5 @@
 from django.http import HttpResponse
-from django.template.loader import 
-
-template = get_template('index.html')
-html = template.render({'user': 'DOC'}, request)
-
 def main_page(request):
-	output = '''
-	    <html>
-	        <head><title>%s</title></head>
-		<body>
-			<h1>%s</h1><p>%s</p>
-		</body>
-	    </html>
-	''' % (
-	  'Test Page',
-	  'Created By Davall Clarke',
-	  html
-	)
-	return HttpResponse(output)
-
+    template = get_template('index.html')
+	html = template.render({'user': 'DOC'}, request)
+    return HttpResponse(html)
