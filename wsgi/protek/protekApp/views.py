a333684e0cc6c5f34056d013b6532b46e3112258
davallclarke <davallclarke@gmail.com>
2015-11-08 17:09:38 -0500

    W7 Debugging

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,14 +1,9 @@
 from django.http import HttpResponse
 from django.template.loader import get_template
 
+def main_page(request):
 template = get_template('index.html')
 html = template.render({'user': 'DOC'}, request)
-
-
-
-pth = os.path.join(os.path.dirname(__file__), 'templates')
-
-def main_page(request):
 	return HttpResponse(html)
 	
 	
