4b1e1983943fa6c14f72cacca8708b00819e9864
davallclarke <davallclarke@gmail.com>
2015-11-08 20:06:40 -0500

    W7 Debugging

--- wsgi/protek/protekApp/views.py
+++ wsgi/protek/protekApp/views.py
@@ -1,28 +1,8 @@
-import sys
-
-try:
-
 from django.http import HttpResponse
-from django.http import Http404
 from django.template.loader import get_template
 
-
-template = get_template('main.html')	
-pth = template.render({'user': 'DOC'})
-
 def main_page(request):
-	output = '''
-	    <html>
-	        <head><title>%s</title></head>
-		<body>
-			<h1>%s</h1><p>%s</p>
-		</body>
-	    </html>
-	''' % (
-	  'Test Page',
-	  'Created By Davall Clarke',
-	  pth
-	)
-	return HttpResponse(output)
-	except:
-		raise Http404("Failed My Youth!!!%s : ",sys.exc_info()[0])
+    template = get_template('home.html')
+	outp = template.render({'user': 'DOC'})
+    return HttpResponse(outp)			
+			
