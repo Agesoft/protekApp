896bcdfe9c6fdfcf2f38d97c0180c55973ded01e
davallclarke <davallclarke@gmail.com>
2015-11-09 01:53:38 -0500

    W7 Debugging

--- wsgi/protek/protek/urls.py
+++ wsgi/protek/protek/urls.py
@@ -28,5 +28,6 @@ from protekApp.views import *
 #from . import views
 
 urlpatterns = [
-    url(r'^$', home, name="home"),
+    url(r'^$', home, name="home"),	
+	url(r'^contact/', contact, name="contact"),
 ]
