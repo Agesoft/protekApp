d1abeb6e825bb43f80e124bcafa8022d17fd7c0b
davallclarke <davallclarke@gmail.com>
2015-11-08 19:16:08 -0500

    W7 Debugging

--- wsgi/protek/protek/settings.py
+++ wsgi/protek/protek/settings.py
@@ -67,7 +67,9 @@ ROOT_URLCONF = 'protek.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [''],
+        'DIRS': [
+				'/var/lib/openshift/563e4a0c2d527145c2000038/app-root/repo/wsgi/protek/protekApp/templates',
+			],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,6 +77,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+				'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
             ],
         },
     },
